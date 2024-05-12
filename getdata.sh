42484626a32795495c1c8cae4dc92b9304b93325
willraf <103647681+willraf@users.noreply.github.com>
2024-05-12 15:49:38 +0200

    comment out wikitext datasets from getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -34,27 +34,27 @@ echo "---"
 mkdir -p data
 cd data
 
-if [[ ! -d 'wikitext-2' ]]; then
-    echo "- Downloading WikiText-2 (WT2)"
-    wget --quiet --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
-    unzip -q wikitext-2-v1.zip
-    cd wikitext-2
-    mv wiki.train.tokens train.txt
-    mv wiki.valid.tokens valid.txt
-    mv wiki.test.tokens test.txt
-    cd ..
-fi
+# if [[ ! -d 'wikitext-2' ]]; then
+#     echo "- Downloading WikiText-2 (WT2)"
+#     wget --quiet --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
+#     unzip -q wikitext-2-v1.zip
+#     cd wikitext-2
+#     mv wiki.train.tokens train.txt
+#     mv wiki.valid.tokens valid.txt
+#     mv wiki.test.tokens test.txt
+#     cd ..
+# fi
 
-echo "- Downloading WikiText-103 (WT2)"
-if [[ ! -d 'wikitext-103' ]]; then
-    wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
-    unzip -q wikitext-103-v1.zip
-    cd wikitext-103
-    mv wiki.train.tokens train.txt
-    mv wiki.valid.tokens valid.txt
-    mv wiki.test.tokens test.txt
-    cd ..
-fi
+# echo "- Downloading WikiText-103 (WT2)"
+# if [[ ! -d 'wikitext-103' ]]; then
+#     wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+#     unzip -q wikitext-103-v1.zip
+#     cd wikitext-103
+#     mv wiki.train.tokens train.txt
+#     mv wiki.valid.tokens valid.txt
+#     mv wiki.test.tokens test.txt
+#     cd ..
+# fi
 
 echo "- Downloading enwik8 (Character)"
 if [[ ! -d 'enwik8' ]]; then
